212a2ab8d1c9eb38be81f99957b3b66ee150a66c
dhanushv2000 <66072205+dhanushv2000@users.noreply.github.com>
2021-03-17 08:54:22 +0530

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import *
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
+
+class CreateSection(ModelForm):
+	class Meta:
+		model = Section
+		fields = '__all__'
+
+class CreateStudent(ModelForm):
+	class Meta:
+		model = Student_link
+		fields = '__all__'
