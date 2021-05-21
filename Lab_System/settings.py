10dd5f905d7f420f3a87b4b8a3ef145231d37575
dhanushv2000 <66072205+dhanushv2000@users.noreply.github.com>
2021-05-21 11:56:21 +0530

    Add files via upload

--- Lab_System/settings.py
+++ Lab_System/settings.py
@@ -138,3 +138,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_URL = '/images/'
